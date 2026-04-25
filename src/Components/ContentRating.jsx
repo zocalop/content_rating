62a698786dcbec5ad01829ec984de693119059a6
James <jcb2861@gmail.com>
2026-04-24 22:36:43 -0300

    Updated ContentRating.jsx in main

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,18 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+         likes: 0,
+         dislikes: 0
+        };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+         <p>Are you ready to traverse the dungeon, and perhaps see the horrors of waking freedom?</p>
+         <div className='rating-buttons'>
+         </div>
+     </div>
     );
   }
 }

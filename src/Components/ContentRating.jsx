64c6630ae34221b344e03b4e9031b598961a9d7d
zocalop <jcb2861@gmail.com>
2026-04-25 12:35:29 -0300

    Updated ContentRating.jsx in main

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,14 +7,30 @@ class ContentRating extends Component {
     super();
     this.state = {
          likes: 0,
-         dislikes: 0
-        };
+         dislikes: 0,
+         handleLike: () => {
+             this.setState((prevState) => ({
+                 likes: prevState.likes + 1
+             }));
+         },
+         handleDislike: () => {
+             this.setState((prevState) => ({
+                 dislikes: prevState.dislikes + 1
+             }));
+         },
+    };
   }
   render() {
     return (
      <div className='content-rating'>
          <p>Are you ready to traverse the dungeon, and perhaps see the horrors of waking freedom?</p>
          <div className='rating-buttons'>
+             <button className="like-button" onClick={this.state.handleLike}>
+                 Like ({this.state.likes})
+             </button>
+             <button className="dislike-button" onClick={this.state.handleDislike}>
+                 Dislike ({this.state.dislikes})
+             </button>
          </div>
      </div>
     );
